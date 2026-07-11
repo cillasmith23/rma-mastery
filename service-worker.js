975a2890b41301e38a12ea381be030ba98238fb4
cillasmith23 <cilla.cypress@gmail.com>
2026-07-11 18:42:28 -0400

    Update to RMA Mastery+ v4.5

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,1 @@
-const CACHE='rma-mastery-plus-v4.4';
-const ASSETS=['./','index.html','styles.css','app.js','manifest.webmanifest','icon.svg','questions-general.json','questions-admin.json','questions-clinical.json'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
-self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
-self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+self.addEventListener('install',()=>self.skipWaiting());
